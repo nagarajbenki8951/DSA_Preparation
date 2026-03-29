070fdeab8b4d000ed7b603e3703c66ca935b29a6
nagarajbenki8951 <nagarajbenki8951@gmail.com>
2026-03-29 20:58:37 +0530

    linked list theory added

---
+++ src/main/java/com/nagaraj/dsa/basic/LinkedList/DoublyLinkedList.java
@@ -0,0 +1,16 @@
+package com.nagaraj.dsa.basic.LinkedList;
+
+public class DoublyLinkedList {
+
+}
+
+class Node{
+    private int val;
+    private Node node;
+    private int size;
+
+    public Node(int val){
+        this.val = val;
+    }
+
+}
